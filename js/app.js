048a131a7b7026e13d34e301a86e1b31eef416c0
Jason Howie <howiejason93@gmail.com>
2018-10-12 12:34:17 -0400

    click event to change cats

--- js/app.js
+++ js/app.js
@@ -4,27 +4,27 @@ let model = {
 	{
 		clickCount: 0,
 		name: 'Mario',
-		img: 'https://www.wwwallaboutcats.com/wp-content/uploads/2016/10/shutterstock_454564069.jpg'
+		img: 'img/mario.jpg'
 	},
 	{
 		clickCount: 0,
 		name: 'Yoshi',
-		img: 'https://www.catster.com/wp-content/uploads/2017/11/Tabby-cat-opener.jpg'
+		img: 'img/yoshi.jpg'
 	},
 	{
 		clickCount: 0,
 		name: 'Luigi',
-		img: 'https://vignette.wikia.nocookie.net/animal-jam-clans-1/images/f/f6/White-cat-breeds.jpg/revision/latest?cb=20161217162830'
+		img: 'img/luigi.jpg'
 	},
 	{
 		clickCount: 0,
 		name: 'Luke',
-		img: 'http://www.ourfriends4ever.com/wp-content/uploads/2017/02/ourfriends4ever_russian_blue_cat2_header.jpg'
+		img: 'img/luke.jpg'
 	},
 	{
 		clickCount: 0,
 		name: 'Obi',
-		img: 'https://www.thesprucepets.com/thmb/q5G2nOmG7Z0afdZnzf1nCSknRHI=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/egypt-59b7224c6f53ba00114fa958-59bae4346f53ba0010439047.jpg'
+		img: 'img/obi.jpg'
 	}
 	]
 };
@@ -118,7 +118,12 @@ let catListView = {
             elem = $('<li></li>');
             elem.html(cat.name);
             
-
+            elem.click((function(catCopy) {
+                return function() {
+                    octopus.setCurrentCat(catCopy);
+                    catView.render();
+                }
+            })(cat))
 
             this.catListElem.append(elem);
         }
